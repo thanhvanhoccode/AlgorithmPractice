e880fd7e7e794237fcaf72df24c155c00d89709f
thanhvanhoccode <thanhvanpycoder@gmail.com>
2025-07-27 21:29:06 +0700

    270725_diagonalOfCross.py

---
+++ 270725_diagonalOfCross.py
@@ -0,0 +1,5 @@
+#https://codelearn.io/training/2196?tab=description&activityId=0&activityType=12&token=
+def diagonal_of_cross(S):
+    from math import sqrt
+    a = sqrt(S/5)
+    return round(a*sqrt(10),1)
